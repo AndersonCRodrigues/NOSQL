b5e1971032f6e35b37e681a0b4a577dc30e7993f
Anderson Costa Rodrigues <anderson.c.rodrigues@hotmail.com>
2026-02-24 20:24:26 -0300

    init fast api server

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def hello():
+    return {"message":"Hello, World!"}
+
+
+if "__main__" == __name__:
+    uvicorn.run(app, host="0.0.0.0", port="8000")
