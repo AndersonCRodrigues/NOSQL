c96a11ebafee04957e0c4e9fe58e24edcc67da57
Anderson Costa Rodrigues <anderson.c.rodrigues@hotmail.com>
2026-02-25 19:55:21 -0300

    feat: config

---
+++ app/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    ENV: str = "development"
+    DATABASE_URL: str = "postgressql://user:pass@localhost:5432/app"
+    REDDIS_URL: str = "redis://localhost:6379"
+
+settings = Settings()
